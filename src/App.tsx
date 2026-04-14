7f0fb5ff5805bad4602a8f4f8544c8c4b8be1403
jeyrich <jeyrich@uiowa.edu>
2026-04-14 10:58:35 -0500

    Test 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 
 /* App.tsx: Main Entry Point, Global State, Video Sync, and Layout Grid       */
 /* -------------------------------------------------------------------------- */
+/*Jack Git test*/
 
 
 import React, { useState, useRef } from 'react';
