a0b9879a68e1894eb4c541cee2f25d021b273f10
Theo Smith <theosmith02@gmail.com>
2026-04-15 11:23:16 -0500

    Comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@
 
 /* App.tsx: Main Entry Point, Global State, Video Sync, and Layout Grid       */
 /* -------------------------------------------------------------------------- */
-/*Jack Git test*/
-/*Theo approves*/
 
 
 import React, { useState, useRef } from 'react';
